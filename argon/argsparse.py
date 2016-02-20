91e150500202904811f872fc01f9d011a7d89013
Vic Garcia <vic.garcia@outlook.com>
2016-02-20 02:17:16 -0600

    create a module for argument parsing

--- argon/argsparse.py
+++ argon/argsparse.py
@@ -1,63 +1,27 @@
 import re
 
-def countdelay(line):
-    ''' parse --count/delay arguments '''
-    # parse count
-    count = 1
+
+def count(line):
+    ''' parse --count argument to an int or None '''
+    count = None
     try:
         match = re.search(r'count=(\d+)', line)
         if match is not None:
             count = int(match.group(1))
     except:
         pass
-    # parse delay
-    delay = 0
+    return count
+
+def delay(line):
+    ''' parse --delay argument to an int or None '''
+    delay = None
     try:
         match = re.search(r'delay=(\d+)', line)
         if match is not None:
             delay = int(match.group(1))
     except:
         pass
-    # enforce a min 3 second delay
-    if count > 1 and delay < 3:
-        delay = 3
-    return ( count, delay )
-
-def location(line):
-    ''' parse --lat/--lng/--alt arguments to a tuple '''
-    # parse latitude
-    latitude = None
-    try:
-        match = re.search(r'lat=(-?\d+.\d+)', line)
-        if match is not None:
-            latitude = float(match.group(1))
-    except:
-        pass
-    # parse longitude
-    longitude = None
-    try:
-        match = re.search(r'lng=(-?\d+.\d+)', line)
-        if match is not None:
-            longitude = float(match.group(1))
-    except:
-        pass
-    # return a two item tuple, (latitude, longitude)
-    if latitude and longitude:
-        return latitude, longitude
-    # return None, None when both are not present
-    else:
-        return None, None
-
-def altitude(line):
-    ''' parse --altitude argument to an int or None '''
-    altitude = None
-    try:
-        match = re.search(r'altitude=(\d+)', line)
-        if match is not None:
-            altitude = float(match.group(1))
-    except:
-        pass
-    return altitude
+    return delay
 
 def altitude(line):
     ''' parse --altitude argument to an int or None '''
@@ -113,4 +77,57 @@ def maximum(line):
     except:
         pass
     return maximum
+
+def latitude(line):
+    ''' parse --latitude argument to a float or None '''
+    latitude = None
+    try:
+        match = re.search(r'latitude=(-?\d+.\d+)', line)
+        if match is not None:
+            latitude = float(match.group(1))
+    except:
+        pass
+    return latitude
+
+def longitude(line):
+    ''' parse --longitude argument to a float or None '''
+    longitude = None
+    try:
+        match = re.search(r'longitude=(-?\d+.\d+)', line)
+        if match is not None:
+            longitude = float(match.group(1))
+    except:
+        pass
+    return longitude
+
+def position(line):
+    ''' parse --lat/--lng/--alt arguments to a tuple '''
+    # parse latitude
+    latitude = None
+    try:
+        match = re.search(r'lat=(-?\d+.\d+)', line)
+        if match is not None:
+            latitude = float(match.group(1))
+    except:
+        pass
+    # parse longitude
+    longitude = None
+    try:
+        match = re.search(r'lng=(-?\d+.\d+)', line)
+        if match is not None:
+            longitude = float(match.group(1))
+    except:
+        pass
+    # parse altitude
+    altitude = None
+    try:
+        match = re.search(r'alt=(\d+)', line)
+        if match is not None:
+            altitude = float(match.group(1))
+    except:
+        pass
+    # only return lat/lng as a pair
+    if latitude == None or longitude == None:
+        latitude, longitude = None, None
+    return latitude, longitude, altitude
 
