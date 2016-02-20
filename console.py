0e5086772f505b25b00e67d35de04ac773b25c09
Vic Garcia <vic.garcia@outlook.com>
2016-02-19 22:14:37 -0600

    create cli entry points

---
+++ console.py
@@ -0,0 +1,6 @@
+from argon.app import App
+from argon.connect import drone
+
+if __name__ == '__main__':
+    App(drone).cmdloop()
+
---
+++ test.py
@@ -0,0 +1,6 @@
+from argon.app import App
+from argon.connect import sitl
+
+if __name__ == '__main__':
+    App(sitl).cmdloop()
+
