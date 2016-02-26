c34b556db8a3360c25c1a2bef04a146af40adc59
Vic Garcia <vic.garcia@outlook.com>
2016-02-26 11:43:10 -0600

    refactor argsparse for DRY

--- argon/argsparse.py
+++ argon/argsparse.py
@@ -1,132 +1,75 @@
 import re
+import logging
 
+
+def _parse_arg_with_regex_to_typ(line, regex, typ):
+    ''' base functionality for argument parsing '''
+    value = None
+    try:
+        match = re.search(regex, line)
+        if match is not None:
+            value = typ(match.group(1))
+    except Exception as error:
+        logging.error('error parsing argument : {}'.format(error.message))
+    return value
 
 def count(line):
-    ''' parse --count argument to an int or None '''
-    count = None
-    try:
-        match = re.search(r'count=(\d+)', line)
-        if match is not None:
-            count = int(match.group(1))
-    except:
-        pass
-    return count
+    ''' parse --count argument to an int '''
+    regex = r'count=(\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, int)
 
 def delay(line):
-    ''' parse --delay argument to an int or None '''
-    delay = None
-    try:
-        match = re.search(r'delay=(\d+)', line)
-        if match is not None:
-            delay = int(match.group(1))
-    except:
-        pass
-    return delay
+    ''' parse --delay argument to an int '''
+    regex = r'delay=(\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, int)
 
 def altitude(line):
-    ''' parse --altitude argument to an int or None '''
-    altitude = None
-    try:
-        match = re.search(r'altitude=(\d+)', line)
-        if match is not None:
-            altitude = float(match.group(1))
-    except:
-        pass
-    return altitude
+    ''' parse --altitude argument to an int '''
+    regex = r'altitude=(\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, int)
 
 def distance(line):
-    ''' parse --distance argument to an int or None '''
-    distance = None
-    try:
-        match = re.search(r'distance=(\d+)', line)
-        if match is not None:
-            distance = int(match.group(1))
-    except:
-        pass
-    return distance
+    ''' parse --distance argument to an int '''
+    regex = r'distance=(\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, int)
 
 def heading(line):
-    ''' parse --heading argument to an int or None '''
-    heading = None
-    try:
-        match = re.search(r'heading=(\d+)', line)
-        if match is not None:
-            heading = int(match.group(1))
-    except:
-        pass
-    return heading
+    ''' parse --heading argument to an int '''
+    regex = r'heading=(\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, int)
 
 def interval(line):
-    ''' parse --interval argument to an int or None '''
-    interval = None
-    try:
-        match = re.search(r'interval=(\d+)', line)
-        if match is not None:
-            interval = int(match.group(1))
-    except:
-        pass
-    return interval
+    ''' parse --interval argument to an int '''
+    regex = r'interval=(\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, int)
 
 def maximum(line):
-    ''' parse --maximum argument to an int or None '''
-    maximum = None
-    try:
-        match = re.search(r'maximum=(\d+)', line)
-        if match is not None:
-            maximum = int(match.group(1))
-    except:
-        pass
-    return maximum
+    ''' parse --maximum argument to an int '''
+    regex = r'maximum=(\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, int)
+
+def radius(line):
+    ''' parse --radius argument to an int '''
+    regex = r'radius=(\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, int)
 
 def latitude(line):
-    ''' parse --latitude argument to a float or None '''
-    latitude = None
-    try:
-        match = re.search(r'latitude=(-?\d+.\d+)', line)
-        if match is not None:
-            latitude = float(match.group(1))
-    except:
-        pass
-    return latitude
+    ''' parse --latitude argument to a float '''
+    regex = r'latitude=(-?\d+.\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, float)
 
 def longitude(line):
-    ''' parse --longitude argument to a float or None '''
-    longitude = None
-    try:
-        match = re.search(r'longitude=(-?\d+.\d+)', line)
-        if match is not None:
-            longitude = float(match.group(1))
-    except:
-        pass
-    return longitude
+    ''' parse --longitude argument to a float '''
+    regex = r'longitude=(-?\d+.\d+)'
+    return _parse_arg_with_regex_to_typ(line, regex, float)
 
 def position(line):
     ''' parse --lat/--lng/--alt arguments to a tuple '''
-    # parse latitude
-    latitude = None
-    try:
-        match = re.search(r'lat=(-?\d+.\d+)', line)
-        if match is not None:
-            latitude = float(match.group(1))
-    except:
-        pass
-    # parse longitude
-    longitude = None
-    try:
-        match = re.search(r'lng=(-?\d+.\d+)', line)
-        if match is not None:
-            longitude = float(match.group(1))
-    except:
-        pass
-    # parse altitude
-    altitude = None
-    try:
-        match = re.search(r'alt=(\d+)', line)
-        if match is not None:
-            altitude = float(match.group(1))
-    except:
-        pass
-    # only return lat/lng as a pair
+    # parse lat/lng/alt as floats, int
+    latitude = _parse_arg_with_regex_to_typ(line, r'lat=(-?\d+.\d+)', float)
+    longitude = _parse_arg_with_regex_to_typ(line, r'lng=(-?\d+.\d+)', float)
+    altitude = _parse_arg_with_regex_to_typ(line, r'alt=(\d+)', int)
+    # only return lat/lng as a pair, otherwise both as None
     if latitude == None or longitude == None:
         latitude, longitude = None, None
     return latitude, longitude, altitude
