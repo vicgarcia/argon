fd1bb15adb3d61b050b442d411d2ccf7d98b4406
Vic Garcia <vic.garcia@outlook.com>
2016-05-05 01:21:48 -0500

    add firmware in version command

--- argon.py
+++ argon.py
@@ -215,7 +215,9 @@ class App(cmd.Cmd):
     def do_version(self, args):
         ''' print current version of argon and vehicle firmware version '''
         self.vehicle.wait_ready('autopilot_version')
-        console.white("argon console : 0.5.0")
+        console.white("console console : 0.5.0")
+        console.white("vehicle firmware : {}".format(self.vehicle.version))
+        console.blank()
 
     def do_exit(self, args):
         ''' close connection to vehicle and exit console environment '''
